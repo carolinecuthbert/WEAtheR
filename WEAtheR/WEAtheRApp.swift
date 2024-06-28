e9f9fbbd4a0b7bc8aecd20a3d7acb54bd88b4dd8
carolinecuthbert <cgcuthbert.07@gmail.com>
2024-06-27 22:57:45 -0700

    Initial Commit

---
+++ WEAtheR/WEAtheRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEAtheRApp.swift
+//  WEAtheR
+//
+//  Created by Caroline Cuthbert on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WEAtheRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
